b6f5a60a55d68bc75f7990ef7f8099e6cddfe888
Dan Gopstein <dan@gopstein.com>
2015-02-05 13:35:47 -0500

    tests pass

--- hw1_above_line.clj
+++ hw1_above_line.clj
@@ -1,32 +1,38 @@
 (defn test-line [f]
-  (let [res (map (fn [arg] [(apply f (first arg)) (last arg)])
+  (let [res (map
+     (fn [[fst snd]]
+        (let [res (apply f fst)]
+        (prn fst res snd)
+        [res snd]))
     '(
       [[[1 0] [2 1] [3 2]] false]
       [[[1 0] [2 1] [2 1]] false]
       [[[1 0] [2 1] [1.5 0.5]] false]
-      [[[1 0] [2 1] [1 0.5]] false]
+      [[[1 0] [2 1] [1 0.5]] true]
       [[[1 0] [2 1] [1 1.5]] true]
       [[[1 0] [2 1] [0 -1.5]] false]
       [[[1 0] [2 1] [0 -0.5]] true]
+      [[[2 1] [1 0] [0 -0.5]] true]
+      [[[1 1] [2 0] [0 -0.5]] false]
+      [[[1 1] [2 0] [0 -5]] false]
+      [[[1 1] [2 0] [0 5]] true]
     )
     )]
 
-    (if (every? = res)
+    (if (every? #(apply = %) res)
       (prn "Success!")
-      (do (prn "Failure!" (prn res))))))
+      (do (prn "Failure!" ;(prn res)
+      )))))
 
-(defn pt-sub [[ax ay] [bx by]] [(- bx ax) (- by ay)])
-
-(defn left-of-line? [a b c]
-  (let [diff (pt-sub (pt-sub a b) (pt-sub a c))]
-    (prn "diff" diff)
-    (> 0 (first diff))))
-  
 (defn above-line? [a b c]
+  (defn pt-sub [[ax ay] [bx by]] [(- bx ax) (- by ay)])
   (let [[ax ay]    a
         [cx cy]    c
         slope (apply / (pt-sub a b))
         y-off (- 0 (* slope ax))]
+    (prn 'y-off y-off)
+    (prn 'slope slope)
+    (prn 'a_cy (+ (* slope cx) y-off))
     (> cy (+ (* slope cx) y-off))))
   
 (def main (test-line above-line?))
