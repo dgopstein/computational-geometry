3e8065249c451955ace17b116d70dbf10824cfd2
Dan Gopstein <dan@gopstein.com>
2015-04-27 21:11:11 -0400

    No direction

--- homework/hw9_domination_tree.clj
+++ homework/hw9_domination_tree.clj
@@ -1,6 +1,19 @@
-(def test-pts '((1 1) (4 2) (2 3) (6 5) (3 6) (7 7)))
+(def test-pts [[1 1] [4 2] [2 3] [6 5] [3 6] [7 7]])
 
-(def draw [pts]
-  (doseq [[x y]] (println (str "(x:" x ", y:" y ")"))))
+(defn value [node] (nth node 0))
+(defn children [node] (nth node 1))
+(defn x [pt] (nth pt 0))
+(defn y [pt] (nth pt 1))
+(defn dominates? [a b] (or (> (x a) (x b)) (> (y a) (y b))))
+
+(defn insert [tree pt]
+  (if (dominates? (value tree) pt)
+    
+
+(defn draw [pts]
+  (doseq [[x y] pts]
+    (prn (str "(x:" x ", y:" y ")"))))
 
 (draw test-pts)
+
+
